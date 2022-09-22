7a284f97c95e7c3390148878138c7b84ea6ede6e
Uliana Le <ulianochka29@mail.ru>
2022-09-22 05:31:02 +0300

    add new test

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -27,6 +27,7 @@ class MoneyTransferTest {
         balanceBefore2 = dashboardPage.getCardBalance(dashboardPage.card2);
     }
 
+    @Order(1)
     @Test
     void shouldTransferMoneyToFirstCard() { // from 2 to 1 card
         amount = balanceBefore2 - 1;
@@ -38,6 +39,7 @@ class MoneyTransferTest {
         Assertions.assertEquals(balanceBefore2 - amount, balanceAfter2);
     }
 
+    @Order(2)
     @Test
     void shouldTransferMoneyToSecondCard() { // from 1 to 2
         amount = balanceBefore1 - 1;
@@ -49,6 +51,31 @@ class MoneyTransferTest {
         Assertions.assertEquals(balanceBefore2 + amount, balanceAfter2);
     }
 
+    @Order(3)
+    @Test
+    void shouldTransferAllMoneyToFirstCard() { // from 2 to 1 card
+        amount = balanceBefore2;
+        dashboardPage.chooseCardButton(dashboardPage.cardButton1)
+                .successTransfer(DataHelper.getCardNumber2(), amount);
+        int balanceAfter1 = dashboardPage.getCardBalance(dashboardPage.card1);
+        int balanceAfter2 = dashboardPage.getCardBalance(dashboardPage.card2);
+        Assertions.assertEquals(balanceBefore1 + amount, balanceAfter1);
+        Assertions.assertEquals(balanceBefore2 - amount, balanceAfter2);
+    }
+
+    @Order(4)
+    @Test
+    void shouldTransferAllMoneyToSecondCard() { // from 1 to 2
+        amount = balanceBefore1;
+        dashboardPage.chooseCardButton(dashboardPage.cardButton2)
+                .successTransfer(DataHelper.getCardNumber1(), amount);
+        int balanceAfter1 = dashboardPage.getCardBalance(dashboardPage.card1);
+        int balanceAfter2 = dashboardPage.getCardBalance(dashboardPage.card2);
+        Assertions.assertEquals(balanceBefore1 - amount, balanceAfter1);
+        Assertions.assertEquals(balanceBefore2 + amount, balanceAfter2);
+    }
+
+    @Order(5)
     @Test
     void shouldErrorTransferMoreThenAvailableToFirstCard() { // from 2 to 1 error
         amount = balanceBefore2 + 1;
@@ -56,13 +83,12 @@ class MoneyTransferTest {
                 .errorTransfer(DataHelper.getCardNumber2(), amount);
     }
 
+    @Order(6)
     @Test
     void shouldErrorTransferMoreThenAvailableToSecondCard() { // from 1 to 2 error
         amount = balanceBefore1 + 1;
         dashboardPage.chooseCardButton(dashboardPage.cardButton2)
                 .errorTransfer(DataHelper.getCardNumber1(), amount);
     }
-
-
 }
 
